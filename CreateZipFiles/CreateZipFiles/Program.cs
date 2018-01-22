8da494ceab0d5f67d89dc843be9142b1a6d2199a
Christian Nagel <christian@christiannagel.com>
2018-01-22 15:52:24 +0100

    change for Professional C# 7

--- CreateZipFiles/CreateZipFiles/Program.cs
+++ CreateZipFiles/CreateZipFiles/Program.cs
@@ -7,21 +7,19 @@ namespace CreateZipFiles
 {
     class Program
     {
-        private const string SourceArchive = @"C:\Users\chris\Downloads\ProfessionalCSharp6-master.zip";
-        private const string VS2015SourceArchive = @"c:\users\chris\downloads\ProfessionalCSharp6-vs2015.zip";
-        private const string SourceTempFolder = @"c:\temp\procsharp";
+        private const string SourceArchive = @"C:\Users\chris\Downloads\ProfessionalCSharp7-master.zip";
+        private const string SourceTempFolder = @"c:\temp\procsharp7";
 
-        private readonly static string VS2015SourcesFolder = Path.Combine(SourceTempFolder, "ProfessionalCSharp6-vs2015");
-        private readonly static string VS2017SourcesFolder = Path.Combine(SourceTempFolder, "ProfessionalCSharp6-master");
+        private readonly static string SourcesFolder = Path.Combine(SourceTempFolder, "ProfessionalCSharp7-master");
 
         private const string ZipFolder = @"c:\temp\zipchapters";
 
-        private const string ResultFolder = @"c:\temp\results";
+       // private const string ResultFolder = @"c:\temp\results";
 
 
         private static Dictionary<string, string> ZipFileMapping;
 
-        static void Main(string[] args)
+        static void Main()
         {
             InitZipFileMapping();
             Run();
@@ -30,76 +28,120 @@ namespace CreateZipFiles
         private static void Run()
         {
             if (!UncompressFiles()) return;
-            ChapterZipFiles();
+            CreateZipFilesForChapters();
 
+            // the following is only needed for multiple versions per zip file, e.g. vs2015 & vs2017
+            //if (!Directory.Exists(ResultFolder))
+            //{
+            //    Directory.CreateDirectory(ResultFolder);
+            //}
 
-            if (!Directory.Exists(ResultFolder))
-            {
-                Directory.CreateDirectory(ResultFolder);
-            }
+            //foreach (var zipFile in ZipFileMapping.Keys)
+            //{
+            //    string zipPath = Path.Combine(ResultFolder, zipFile);
 
-            foreach (var zipFile in ZipFileMapping.Keys)
-            {
-                string zipPath = Path.Combine(ResultFolder, zipFile);
+            //    string file1 = Path.Combine(ZipFolder, $"{ZipFileMapping[zipFile]}-vs2015.zip");
+            //    string file2 = Path.Combine(ZipFolder, $"{ZipFileMapping[zipFile]}-vs2017.zip");
 
-                string file1 = Path.Combine(ZipFolder, $"{ZipFileMapping[zipFile]}-vs2015.zip");
-                string file2 = Path.Combine(ZipFolder, $"{ZipFileMapping[zipFile]}-vs2017.zip");
+            //    using (ZipArchive archive = ZipFile.Open(zipPath, ZipArchiveMode.Create))
+            //    {
+            //        if (File.Exists(file1))
+            //        {
+            //            archive.CreateEntryFromFile(file1, Path.GetFileName(file1));
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine($"{file1} does not exist");
+            //        }
 
-                using (ZipArchive archive = ZipFile.Open(zipPath, ZipArchiveMode.Create))
-                {
-                    if (File.Exists(file1))
-                    {
-                        archive.CreateEntryFromFile(file1, Path.GetFileName(file1));
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{file1} does not exist");
-                    }
+            //        if (File.Exists(file2))
+            //        {
+            //            archive.CreateEntryFromFile(file2, Path.GetFileName(file2));
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine($"{file2} does not exist");
+            //        }
+            //    }
 
-                    if (File.Exists(file2))
-                    {
-                        archive.CreateEntryFromFile(file2, Path.GetFileName(file2));
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{file2} does not exist");
-                    }
-                }
+            //    Console.WriteLine($"created {zipPath}");
 
-                Console.WriteLine($"created {zipPath}");
-
-            }
+            //}
         }
 
-        private static void ChapterZipFiles()
+        private static void CreateZipFilesForChapters()
         {
             if (!Directory.Exists(ZipFolder))
             {
                 Directory.CreateDirectory(ZipFolder);
             }
 
-            void CreateZipFile(string sourceFolder, string zipFile)
-            {
-                if (!Directory.Exists(sourceFolder))
-                {
-                    Console.WriteLine($"{sourceFolder} does not exist");
-                    return;
-                }
-                string zipPath = Path.Combine(ZipFolder, zipFile);
-                ZipFile.CreateFromDirectory(sourceFolder, zipPath);
-                Console.WriteLine($"created {zipPath}");
-            }
-
             foreach (var zipFile in ZipFileMapping.Keys)
             {
-                string chapterFolder = ZipFileMapping[zipFile];
-                string sourceFolder = Path.Combine(VS2015SourcesFolder, chapterFolder);
+                string foldersForChapter = ZipFileMapping[zipFile];
 
-                CreateZipFile(sourceFolder, $"{chapterFolder}-vs2015.zip");
-                sourceFolder = Path.Combine(VS2017SourcesFolder, ZipFileMapping[zipFile]);
-                CreateZipFile(sourceFolder, $"{chapterFolder}-vs2017.zip");
+                // multiple folders for one zip file
+                if (foldersForChapter.Contains(";"))
+                {
+                    string[] folders = foldersForChapter.Split(";");
+                    // CreateZipFile($"{folders[0]}.zip", folders);
+                    CreateZipFile($"{zipFile}", folders);
+                }
+                else
+                {
+                    CreateZipFile($"{zipFile}", foldersForChapter);
+                    // CreateZipFile($"{foldersForChapter}.zip", foldersForChapter);
+                }
             }
 
+            void CreateZipFile(string zipFile, params string[] sourceFolders)
+            {
+                foreach (var sourceFolder in sourceFolders)
+                {
+                    string sourcePath = Path.Combine(SourcesFolder, sourceFolder);
+                    if (!Directory.Exists(sourcePath))
+                    {
+                        Console.WriteLine($"{sourcePath} does not exist");
+                        return;
+                    }
+                }
+
+                if (sourceFolders.Length == 1)
+                {
+                    string sourcePath = Path.Combine(SourcesFolder, sourceFolders[0]);
+                    string zipPath = Path.Combine(ZipFolder, zipFile);
+                    ZipFile.CreateFromDirectory(sourcePath, zipPath);
+                    Console.WriteLine($"created {zipPath}");
+                }
+                else
+                {
+                    string tempFolderPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                    Directory.CreateDirectory(tempFolderPath);
+                    foreach (var sourceFolder in sourceFolders)
+                    {
+                        string subFolder = Path.Combine(tempFolderPath, sourceFolder);
+                        string sourcePath = Path.Combine(SourcesFolder, sourceFolder);
+                        string destinationPath = Path.Combine(tempFolderPath, sourceFolder);
+                        Directory.Move(sourcePath, destinationPath);       
+                    }
+
+                    string zipPath = Path.Combine(ZipFolder, zipFile);
+                    ZipFile.CreateFromDirectory(tempFolderPath, zipPath);
+                    Console.WriteLine($"created {zipPath}");
+
+                    // move back - some files might be needed again
+                    foreach (var sourceFolder in sourceFolders)
+                    {
+                        string subFolder = Path.Combine(tempFolderPath, sourceFolder);
+                        string sourcePath = Path.Combine(SourcesFolder, sourceFolder);
+                        string destinationPath = Path.Combine(tempFolderPath, sourceFolder);
+                        Directory.Move(destinationPath, sourcePath);
+                    }
+
+                    Directory.Delete(tempFolderPath);
+                    Console.WriteLine($"deleted {tempFolderPath}");
+                }
+            }
         }
 
         private static bool UncompressFiles()
@@ -113,10 +155,8 @@ namespace CreateZipFiles
 
             Directory.CreateDirectory(SourceTempFolder);
 
-            Console.WriteLine("extracting VS2017 zip...");
+            Console.WriteLine("extracting zip with all sources...");
             ZipFile.ExtractToDirectory(SourceArchive, SourceTempFolder);
-            Console.WriteLine("extracting VS2015 zip...");
-            ZipFile.ExtractToDirectory(VS2015SourceArchive, SourceTempFolder);
 
             Console.WriteLine($"Uncompressed files to {SourceTempFolder}");
             return true;
@@ -129,50 +169,44 @@ namespace CreateZipFiles
                 ["01_code.zip"] = "HelloWorld",
                 ["02_code.zip"] = "CoreCSharp",
                 ["03_code.zip"] = "ObjectsAndTypes",
-                ["04_code.zip"] = "Inheritance",
-                ["05_code.zip"] = "Resources",
-                ["06_code.zip"] = "Generics",
+                ["04_code.zip"] = "ObjectOrientation",
+                ["05_code.zip"] = "Generics",
+                ["06_code.zip"] = "OperatorsAndCasts",
                 ["07_code.zip"] = "Arrays",
-                ["08_code.zip"] = "OperatorsAndCasts",
-                ["09_code.zip"] = "Delegates",
-                ["10_code.zip"] = "StringsAndRegularExpressions",
-                ["11_code.zip"] = "Collections",
-                ["12_code.zip"] = "SpecialCollections",
-                ["13_code.zip"] = "LINQ",
+                ["08_code.zip"] = "Delegates",
+                ["09_code.zip"] = "StringsAndRegularExpressions",
+                ["10_code.zip"] = "Collections",
+                ["11_code.zip"] = "SpecialCollections",
+                ["12_code.zip"] = "LINQ",
+                ["13_code.zip"] = "FunctionalProgramming",
                 ["14_code.zip"] = "ErrorsAndExceptions",
                 ["15_code.zip"] = "Async",
                 ["16_code.zip"] = "ReflectionAndDynamic",
-                ["17_code.zip"] = "VisualStudio2015",
-                ["18_code.zip"] = "CompilerPlatform",
-                ["19_code.zip"] = "Testing",
-                ["20_code.zip"] = "Diagnostics",
-                ["21_code.zip"] = "Parallel",
-                ["22_code.zip"] = "Synchronization",
-                ["23_code.zip"] = "FilesAndStreams",
+                ["17_code.zip"] = "Memory",
+                ["18_code.zip"] = "VisualStudio",
+                ["19_code.zip"] = "Libraries",
+                ["20_code.zip"] = "DependencyInjection",
+                ["21_code.zip"] = "Tasks",
+                ["22_code.zip"] = "FilesAndStreams",
+                ["23_code.zip"] = "Networking",
                 ["24_code.zip"] = "Security",
-                ["25_code.zip"] = "Networking",
-                ["26_code.zip"] = "Composition",
-                ["27_code.zip"] = "XMLandJSON",
-                ["28_code.zip"] = "Localization",
-                ["29_code.zip"] = "XAML",
-                ["30_code.zip"] = "StylesAndResources",
-                ["31_code.zip"] = "Patterns",
-                ["32_code.zip"] = "WindowsApps",
-                ["33_code.zip"] = "AdvancedWindowsApps",
-                ["34_code.zip"] = "WPF",
-                ["35_code.zip"] = "WPFDocuments",
-                ["36_code.zip"] = "DeploymentWindows",
-                ["37_code.zip"] = "ADONET",
-                ["38_code.zip"] = "EntityFramework",
-                ["39_code.zip"] = "Services",
-                ["40_code.zip"] = "ASPNET",
-                ["41_code.zip"] = "ASPNETMVC",
-                ["42_code.zip"] = "WebAPI",
-                ["43_code.zip"] = "SignalRAndWebHooks",
-                ["44_code.zip"] = "WCF",
-                ["45_code.zip"] = "DeploymentWeb",
-                ["46_code.zip"] = "csproj",
-                ["47_code.zip"] = "CSharp7"
+                ["25_code.zip"] = "Composition",
+                ["26_code.zip"] = "XMLandJSON",
+                ["27_code.zip"] = "ADONet",
+                ["28_code.zip"] = "EFCore",
+                ["29_code.zip"] = "Localization",
+                ["30_code.zip"] = "Tests",
+                ["31_code.zip"] = "Diagnostics",
+                ["32_code.zip"] = "ASPNETCore",
+                ["33_code.zip"] = "MVC",
+                ["34_code.zip"] = "API",
+                ["35_code.zip"] = "SignalRAndWebHooks",
+                ["36_code.zip"] = "BotAndCognitive",
+                ["37_code.zip"] = "Windows",
+                ["38_code.zip"] = "Patterns;PatternsXamarinShared",
+                ["39_code.zip"] = "Styles",
+                ["40_code.zip"] = "AdvancedWindows",
+                ["41_code.zip"] = "Xamarin;PatternsXamarinShared"
             };
         }
     }
